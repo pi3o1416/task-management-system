abcae619d7a87af04581980456d8f5a8824922ae
Monir Hossain <mmonir.m.29@gmail.com>
2023-08-04 11:31:30 +0600

    Add static root.

--- core/settings/base.py
+++ core/settings/base.py
@@ -10,11 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+import os
 from environ import Env
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+from . import BASE_DIR
 
 
 # Environment Config
@@ -126,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
