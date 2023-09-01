737118fc82172ea1ade9cf49b8aa032e964f9323
Monir Hossain <mmonir.m.29@gmail.com>
2023-09-01 22:17:19 +0600

    Add AdminUserFactory.

--- users/factories.py
+++ users/factories.py
@@ -12,8 +12,14 @@ class CustomUserFactory(DjangoModelFactory):
     email = factory.LazyAttribute(
         lambda a: '{first_name}.{last_name}@gmail.com'.format(first_name=a.first_name, last_name=a.last_name)
     )
+    username = factory.Faker('username')
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
     password = factory.django.Password('1234')
     is_email_verified = True
     is_active = True
+    is_staff = False
+
+
+class AdminUserFactory(CustomUserFactory):
+    is_staff = False
